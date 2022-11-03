2f4908d242e1de035346eef9ccb39bb32cd6c6bb
neil dela cruz <genoznightmare@gmail.com>
2022-11-03 22:20:12 +0800

    useSelector, useDispatch hooks for connect in store

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+      <HooksCakeContainer/>
       </div>
     </Provider>
   );
