b51afc89af654f344f3d1230e9f428882d984441
neil dela cruz <genoznightmare@gmail.com>
2022-11-05 22:09:28 +0800

    useSelector, useDispatch

--- react-rtk-vite/src/features/cake/CakeView.jsx
+++ react-rtk-vite/src/features/cake/CakeView.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./cakeSlice";
 
 function CakeView() {
+  const numberOfCakes = useSelector((state) => state.cake.numOfCakes);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h2>Number of Cakes-</h2>
-      <button>Order</button>
-      <button>Restock</button>
+      <h2>Number of Cakes- {numberOfCakes}</h2>
+      <button onClick={() => dispatch(ordered())}>Order</button>
+      <button onClick={() => dispatch(restocked(3))}>Restock</button>
     </div>
   );
 }
--- react-rtk-vite/src/features/icecream/IceCreamView.jsx
+++ react-rtk-vite/src/features/icecream/IceCreamView.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./icecreamSlice";
 
 function IceCreamView() {
+  const [number, setNumber] = useState(1);
+  const numberOfIceCreams = useSelector(
+    (state) => state.icecream.numberOfIceCreams
+  );
+
+  const dispatch = useDispatch();
+  console.log(number);
   return (
     <div>
-      <h2>Number of Ice Cream-</h2>
-      <button>Order</button>
-      <button>Restock</button>
+      <h2>Number of Ice Cream- {numberOfIceCreams}</h2>
+      <button onClick={() => dispatch(ordered())}>Order</button>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={() => dispatch(restocked(parseInt(number)))}>
+        Restock
+      </button>
     </div>
   );
 }
