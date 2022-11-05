c542d85c0da93c09713f62ea03a8c631a86b6e60
neil dela cruz <genoznightmare@gmail.com>
2022-11-05 15:05:46 +0800

    extra reducers

--- rtk-plain/features/icecream/icecreamSlice.js
+++ rtk-plain/features/icecream/icecreamSlice.js
@@ -1,4 +1,5 @@
 const createSlice = require("@reduxjs/toolkit").createSlice;
+const { cakeActions } = require("../cake/cakeSlice");
 
 const initialState = {
   numberOfIceCreams: 20,
@@ -14,6 +15,17 @@ const icecreamSlice = createSlice({
     restocked: (state, action) => {
       state.numberOfIceCreams += action.payload;
     },
+  },
+  //   extraReducers: {
+  //     ["cake/ordered"]: (state) => {
+  //       state.numberOfIceCreams--;
+  //     },
+  //   },
+
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state) => {
+      state.numberOfIceCreams--;
+    });
   },
 });
 
