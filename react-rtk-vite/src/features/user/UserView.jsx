9eba8e9cc979f41db322b724e69851fd585f851c
neil dela cruz <genoznightmare@gmail.com>
2022-11-05 22:50:14 +0800

    fetching data in UserView

--- react-rtk-vite/src/features/user/UserView.jsx
+++ react-rtk-vite/src/features/user/UserView.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
 
 function UserView() {
+  const userData = useSelector((state) => state.user);
+  const { loading, users, errors } = userData;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
       <h2>List of Users</h2>
+      {loading && <Loading />}
+      {errors && <Errors error={errors} />}
+      <DisplayUsers users={users} />
     </div>
-  )
+  );
 }
 
-export default UserView
+function Loading() {
+  return <div>Loading....</div>;
+}
+
+function Errors({ error }) {
+  return <div>{error}</div>;
+}
+
+function DisplayUsers({ users }) {
+  return (
+    <div>
+      {users?.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default UserView;
